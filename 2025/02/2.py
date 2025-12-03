f759b54350ab1236e838675399006a9eaf570903
João Cavalcante <cavalcante.joao@protonmail.ch>
2025-12-02 22:21:42 -0300

    dia 2 2025

--- 2025/02/2.py
+++ 2025/02/2.py
@@ -4,17 +4,31 @@ with open('entrada.txt', 'r') as f:
 		for s in f.read().split(',')
 		]
 
+def get_divisores(valor):
+	divisores = set()
+	for i in range(1, int(valor ** (1/2) + 1)):
+		if valor % i == 0:
+			divisores.add(i)
+			divisores.add(valor // i)
+			
+	return divisores
+
+def dividir_identificador(identificador, divisor):
+	partes = []
+	for i in range(0, len(identificador), divisor):
+		partes.append(identificador[i:i + divisor])
+	return partes
 
 soma_invalidos = 0
 for intervalo in intervalos:
 	for i in range(intervalo[0], intervalo[1] + 1):
 		id_str = str(i)
 		tamanho = len(id_str)
-		if (
-		(tamanho % 2 != 0) or
-		id_str[0:tamanho // 2] != id_str[tamanho // 2: tamanho]
-		):
-			continue
-		soma_invalidos += i
+		divisores = get_divisores(tamanho)
+		for divisor in divisores:
+			partes = set(dividir_identificador(id_str, divisor))
+			if divisor != tamanho and len(partes) == 1:
+				soma_invalidos += i
+				break
 print(soma_invalidos)
-		
+	
