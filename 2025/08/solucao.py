034dc9c169ed72c3482cb044e04f73a72d3b1632
João Cavalcante <cavalcante.joao@protonmail.ch>
2026-01-11 12:19:50 -0300

    solucao com implementacao ingenua de conjuntos disjuntos

--- 2025/08/solucao.py
+++ 2025/08/solucao.py
@@ -1,6 +1,27 @@
-from math import sqrt
+class ConjuntosDisjuntos:
+    def __init__(self, n:int):
+        self.ancestrais = [i for i in range(n)]
+        self.tamanhos = [1 for i in range(n)]
 
-class Ponto():
+    def unir(self, a, b):
+        raiz_a = self.find(a)
+        raiz_b = self.find(b)
+        if raiz_a == raiz_b:
+            return 0
+        self.ancestrais[raiz_a] = raiz_b
+        self.tamanhos[raiz_b] += self.tamanhos[raiz_a]
+
+    def eh_representante(self, i):
+        return i == self.ancestrais[i]
+
+    def find(self, i):
+        if self.eh_representante(i):
+            return i
+        pai = self.ancestrais[i]
+        return self.find(pai)
+
+
+class Ponto:
     def __init__(self, x: int, y: int, z: int):
         self.x = x
         self.y = y
@@ -18,9 +39,9 @@ class Ponto():
         
     def dist(self, p) -> float:
         return (
-            sqrt(
+            (
             (self.x - p.x)**2 + (self.y - p.y)**2 + (self.z - p.z)**2
-            )
+            ) ** (1/2)
         )
 
 
@@ -42,71 +63,53 @@ n deu certo, tem que ser uma estrutura compartilhada mesmo
 n deu certo de novo, gerenciar estado e muito dificil
 
 teria como fazer sem a redundancia? TODO
+
+DSU/UnionFind -> parece ser a solucao
 """
 
-        
-def menor_distancia(p: Ponto, pontos_comp: list) -> Ponto:
-    menor_dist = p.dist(pontos_comp[0])
-    ponto_menor_dist = pontos_comp[0]
-    for i in range(1, len(pontos)):
-        ponto_atual = pontos_comp[i]
-        dist_atual = p.dist(ponto_atual)
-        if dist_atual < menor_dist:
-            menor_dist = dist_atual
-            ponto_menor_dist = ponto_atual
-    return ponto_menor_dist
-        
 #10 -> (n) * (n - 1)/2
 #   -> (n**2 - n)/2
 def get_distancias_ordenadas(pontos):
     distancias = []
     for i in range(len(pontos) - 1):
         for k in range(i + 1, len(pontos)):
-            dist = (pontos[i].dist(pontos[k]), pontos[i], pontos[k])
+            dist = (pontos[i].dist(pontos[k]), i, k)
             distancias.append(dist)
     return sorted(distancias)
             
 
-def conectar_circuitos(ponto_1, ponto_2) -> int:
-    if ponto_1 in ponto_2.circuito:
-        return ponto_1.circuito
-
-    for ponto in ponto_2.circuito:
-        ponto_1.circuito.add(ponto)
-    for ponto in ponto_1.circuito:
-        ponto.circuito = ponto_1.circuito
-
-    return ponto_1.circuito
- 
-
-def get_todos_circuitos(pontos):
-    pontos_circuitos = set()
-    circuitos = []
-    for ponto in pontos:
-        if ponto in pontos_circuitos:
-            continue
-        circuitos.append(ponto.circuito)
-        pontos_circuitos |= ponto.circuito
-    return sorted(circuitos, key=len, reverse=True)
-
 def solucao_1():
     distancias = get_distancias_ordenadas(pontos)
+    conjuntos = ConjuntosDisjuntos(1000)
     for i in range(1000):
-        _, ponto_1, ponto_2 = distancias[i]
-        conectar_circuitos(ponto_1, ponto_2)
-    circuitos = get_todos_circuitos(pontos)
+        _, idx_ponto_1, idx_ponto_2 = distancias[i]
+        conjuntos.unir(idx_ponto_1, idx_ponto_2)
+    top_3 = sorted(
+            [ conjuntos.tamanhos[i] if conjuntos.eh_representante(i) else 0 for i in range(len(conjuntos.tamanhos)) ],
+            reverse=True
+            )[:3]
     total = 1
-    for i in range(3):
-        total *= len(circuitos[i])
+    for tam in top_3:
+        total *= tam
     return total
 
+
 def solucao_2():
+    def tem_unico_ancestral(conjuntos):
+        quant_repr = 0
+        for i in range(len(conjuntos.ancestrais)):
+            quant_repr += conjuntos.eh_representante(i)
+            if quant_repr > 1:
+                return False
+        return True
+
     distancias = get_distancias_ordenadas(pontos)
-    for _, ponto_1, ponto_2 in distancias:
-        circuito = conectar_circuitos(ponto_1, ponto_2)
-        circuitos = get_todos_circuitos(pontos)
-        if len(circuitos) == 1:
-            return (ponto_1.x * ponto_2.x)
+    conjuntos = ConjuntosDisjuntos(len(pontos))
+    for _, idx_ponto_1, idx_ponto_2 in distancias:
+        conjuntos.unir(idx_ponto_1, idx_ponto_2)
+        if not tem_unico_ancestral(conjuntos):
+            continue
+        return (pontos[idx_ponto_1].x * pontos[idx_ponto_2].x)
  
 
 if __name__ == '__main__':
@@ -118,5 +121,3 @@ if __name__ == '__main__':
         print(solucao_1())
         print(solucao_2())
 
-
-      
